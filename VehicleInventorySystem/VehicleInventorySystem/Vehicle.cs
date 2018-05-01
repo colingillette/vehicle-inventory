ecd32777e13e5e415628246e789e484af167ee6e
Colin Gillette <colingillette35@gmail.com>
2018-04-30 23:51:35 -0500

    init commit

---
+++ VehicleInventorySystem/VehicleInventorySystem/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInventorySystem
+{
+    class Vehicle
+    {
+        private string _id;
+        private string _year;
+        private string _make;
+        private string _model;
+        private int _mileage;
+        private string _vin;
+
+        public Vehicle(string id, string year, string make, string model, int mileage, string vin)
+        {
+
+        }
+    }
+}
